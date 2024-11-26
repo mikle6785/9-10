4c70b2835d85e05f386ba315bf5f037a95e49780
mikle6785 <mikle4546@gmail.com>
2024-11-26 13:08:46 +0300

    Add files via upload

---
+++ 9-10 1.5.pas
@@ -0,0 +1,81 @@
+﻿program ArrayManipulation;
+
+const
+  MAX_SIZE = 100;
+
+var
+  arr1, arr2: array[1..MAX_SIZE] of Integer;
+  i, size1, size2: Integer;
+  maxIndex, minIndex: Integer;
+  hasMultipleOfFive: Boolean;
+
+begin3
+
+
+  // Ввод первого массива
+  Write('Введите размер первого массива: ');
+  ReadLn(size1);
+  WriteLn('Введите элементы первого массива:');
+  for i := 1 to size1 do
+  begin
+    Read(arr1[i]);
+  end;
+
+  // Ввод второго массива
+  Write('Введите размер второго массива: ');
+  ReadLn(size2);
+  WriteLn('Введите элементы второго массива:');
+  for i := 1 to size2 do
+  begin
+    Read(arr2[i]);
+  end;
+
+  // Обработка первого массива
+  hasMultipleOfFive := False;
+  maxIndex := 1;
+  
+  for i := 1 to size1 do
+  begin
+    if (arr1[i] mod 5 = 0) and not hasMultipleOfFive then
+    begin
+      hasMultipleOfFive := True;
+    end;
+    
+    if arr1[i] > arr1[maxIndex] then
+      maxIndex := i;
+  end;
+
+  if hasMultipleOfFive then
+    arr1[maxIndex] := 0;
+
+  // Обработка второго массива
+  minIndex := 1;
+  
+  for i := 1 to size2 do
+  begin
+    if arr2[i] < arr2[minIndex] then
+      minIndex := i;
+  end;
+
+  for i := minIndex + 1 to size2 do
+  begin
+    arr2[i] := arr2[i] * 2;
+  end;
+
+  // Вывод результатов
+  WriteLn('Первый массив после обработки:');
+  for i := 1 to size1 do
+  begin
+    Write(arr1[i], ' ');
+  end;
+  WriteLn;
+
+  WriteLn('Второй массив после обработки:');
+  for i := 1 to size2 do
+  begin
+    Write(arr2[i], ' ');
+  end;
+  WriteLn;
+
+  ReadLn;
+end.
---
+++ 9-10-2.5.pas
@@ -0,0 +1,60 @@
+﻿program TwoDimensionalArray;
+
+const
+  ROWS = 8;
+  COLS = 8;
+
+var
+  matrix: array[1..ROWS, 1..COLS] of Integer;
+  resultArray: array[1..ROWS] of Integer;
+  i, j, maxCount, maxValue: Integer;
+
+begin
+  // Инициализация двумерного массива случайными числами
+  Randomize;
+  WriteLn('Исходный двумерный массив:');
+  for i := 1 to ROWS do
+  begin
+    for j := 1 to COLS do
+    begin
+      matrix[i, j] := Random(100); // Генерация случайных чисел от 0 до 99
+      Write(matrix[i, j]:4);
+    end;
+    WriteLn;
+  end;
+
+  // Обработка каждой строки массива
+  for i := 1 to ROWS do
+  begin
+    maxValue := matrix[i, 1];
+    maxCount := 1;
+
+    // Поиск максимального элемента и подсчет его количества
+    for j := 2 to COLS do
+    begin
+      if matrix[i, j] > maxValue then
+      begin
+        maxValue := matrix[i, j];
+        maxCount := 1; // Сброс счетчика, так как найден новый максимум
+      end
+      else if matrix[i, j] = maxValue then
+      begin
+        Inc(maxCount); // Увеличиваем счетчик, если нашли еще один максимум
+      end;
+    end;
+
+    // Заполнение одномерного массива
+    if maxCount = 1 then
+      resultArray[i] := 1
+    else
+      resultArray[i] := -1;
+  end;
+
+  // Вывод результата
+  WriteLn('Результирующий одномерный массив:');
+  for i := 1 to ROWS do
+  begin
+    Write(resultArray[i]:4);
+  end;
+
+end.
